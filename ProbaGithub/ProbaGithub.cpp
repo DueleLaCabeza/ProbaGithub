652555ffda291bcf613da02a77c5315ee0dc73dd
tfb123 <mil.xomiak@gmail.com>
2024-02-10 18:02:27 +0200

    eee

--- ProbaGithub/ProbaGithub.cpp
+++ ProbaGithub/ProbaGithub.cpp
@@ -4,7 +4,13 @@ int main()
     std::cout << "Privet Milan!\n";
     std::cout << "Privet Milan!\n";
     std::cout << "Privet 111111111111111111111111111!\n";
-    a vot tut e cto-to
+    std::cout << "Privet Milan!\n";
+    std::cout << "Privet Milan!\n";
+    std::cout << "Privet Milan!\n";
+    std::cout << "Privet Milan!\n";
+    std::cout << "Privet Milan!\n";
+    std::cout << "Privet Milan!\n";
+    
 
 
 }
