e72b90203ac6c87fdf9805e17b8062b23aac6376
Yehor Shvachko <duelelacabezast@gmail.com>
2024-02-10 17:34:48 +0100

    Revert from u4

--- ProbaGithub/ProbaGithub.cpp
+++ ProbaGithub/ProbaGithub.cpp
@@ -2,14 +2,6 @@
 int main()
 {
     std::cout << "Privet Milan!\n";
-    char bukvi[] = "abs";
-    int a=0;
-    
-    
-	for (int i = 0; i < 3; i++)
-	{
-		std::cout << "Privet Milan!\n";
-	}
 
 }
 
