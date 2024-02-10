6271b1fb6d0b564c9e5e5c96f6940c9e853108c8
Milanas Orševskis <mil.xomiak@gmail.com>
2024-02-10 18:21:29 +0200

    Update ProbaGithub.cpp

--- ProbaGithub/ProbaGithub.cpp
+++ ProbaGithub/ProbaGithub.cpp
@@ -8,8 +8,11 @@ int main()
     std::cout << "Privet Milan!\n";
     std::cout << "Privet Milan!\n";
     std::cout << "Privet Milan!\n";
+    std::cout << "Privet 111111111111111111111111111!\n";
     std::cout << "Privet Milan!\n";
     std::cout << "Privet Milan!\n";
+    std::cout << "Privet 111111111111111111111111111!\n";
+    std::cout << "Privet 111111111111111111111111111!\n";
     
 
 
